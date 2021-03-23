6aefb88af37054e55033177204ed6fd810717fb8
alex <44968616+nderkim@users.noreply.github.com>
2021-03-23 19:25:10 +1100

    fix clean script

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,45 +1,42 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const ignoreDirectories = [".git"];
-const directories = ["node_modules", "dist", "build"];
-const files = [
-  // "package-lock.json",
-  // "yarn.lock",
-];
+const deleteList = ["dist", "build", "node_modules"];
+const ignoreList = [".git"];
 
-async function cleanDirent(dirent, direntPath) {
-  if (dirent.isDirectory()) {
-    if (ignoreDirectories.includes(dirent.name)) {
-      console.log("skip", direntPath);
-      return;
-    } else if (directories.includes(dirent.name)) {
-      console.log("delete", direntPath);
-      return fs.rm(direntPath, { recursive: true });
-    } else {
-      return cleanDir(direntPath);
-    }
-  } else if (dirent.isFile()) {
-    if (files.includes(dirent.name)) {
-      console.log("delete", direntPath);
-      return fs.rm(direntPath);
-    }
+const cleanDirent = (dirPath) => async (dirent) => {
+  const direntPath = path.join(dirPath, dirent.name);
+  if (ignoreList.includes(direntPath)) {
+    console.log("skip", direntPath);
+    return dirent;
+  } else if (deleteList.includes(direntPath)) {
+    console.log("delete", direntPath);
+    await fs.rm(direntPath, { recursive: true });
+    return null;
+  } else if (dirent.isDirectory()) {
+    return (await cleanDir(direntPath)) && dirent;
+  } else {
+    return dirent;
   }
-}
+};
 
 async function cleanDir(dirPath) {
-  const dirents = await fs.readdir(dirPath, { withFileTypes: true });
-  await Promise.all(
-    dirents.map((dirent) =>
-      cleanDirent(dirent, path.join(dirPath, dirent.name))
+  const dirents = (
+    await Promise.all(
+      (await fs.readdir(dirPath, { withFileTypes: true })).map(
+        cleanDirent(dirPath)
+      )
     )
-  );
+  ).filter(Boolean);
   if (dirents.length === 0) {
     console.log("delete", dirPath);
     await fs.rm(dirPath, { recursive: true });
+    return null;
+  } else {
+    return dirents;
   }
 }
 
-const cwd = process.cwd();
-console.log("cwd", cwd);
-cleanDir(cwd);
+process.chdir(path.join(__dirname, ".."));
+console.log("cwd", process.cwd());
+cleanDir(".");
