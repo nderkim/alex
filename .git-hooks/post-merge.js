135b81bfcccd9b94491a256601f06c37781dbadb
alex <alex@nder.kim>
2021-04-07 23:04:44 +1000

    add ansi escape code

--- .git-hooks/post-merge.js
+++ .git-hooks/post-merge.js
@@ -2,6 +2,8 @@ const crypto = require("crypto");
 const fs = require("fs");
 const path = require("path");
 
+const console = require("../lib/common/console");
+
 const readStreamToBuffer = async (stream) => {
   const chunks = [];
   for await (const chunk of stream) chunks.push(chunk);
@@ -19,8 +21,23 @@ const getFileHash = (filePath) => {
   return getStreamHash(fileStream);
 };
 
+// TODO:
+// - buffer compare
+// - stream compare
+
 (async () => {
   const filePath = path.resolve("./package.json"); // relative to cwd
-  const hash = await getFileHash(filePath);
-  console.log(hash);
+  const hashPath = path.resolve("./package.json.hash"); // relative to cwd
+
+  const expectedHash = await fs.promises.readFile(hashPath).catch((err) => {
+    if (err.code === "ENOENT") return null;
+    throw err;
+  });
+
+  if (!expectedHash) return console.error("Hash not found. Please run 'npm i'");
+
+  const actualHash = await getFileHash(filePath);
+
+  if (actualHash.toString() !== expectedHash.toString())
+    console.warn("Your dependencies may have changed. Please run 'npm i'");
 })();
